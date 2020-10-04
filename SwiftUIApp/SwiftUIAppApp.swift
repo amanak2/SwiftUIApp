474f85afb640fcbdc805e12a5547f897c0c0344d
Aman Chawla <aman.chawla96@gmail.com>
2020-10-04 11:50:24 +0530

    Initial commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Aman Chawla on 04/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
